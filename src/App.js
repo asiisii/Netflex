b90d6b0aebf14ab82ea6b9e436e16cd122c702bd
Ashish Malla <ashishmalla45@gmail.com>
2021-05-12 15:08:14 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <h1>It's working</h1>
+    )
+  }
+}
+
+export default App;
