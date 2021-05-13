4755fef95d3d6a4ecb545d19402f9ce3b5e954e5
schlanjo <schlanjo@gmail.com>
2021-05-13 00:05:38 -0600

    Add container for preview image and info, add max width

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -3,11 +3,13 @@ import './preview.css'
 
 export default function Preview() {
   return (
-    <section className="trending-container">
-      <img className="top-img" src="https://observer.case.edu/wp-content/uploads/2020/09/Mulan-900x506.jpg" alt="" />
-      <div className="info">
-        <h1 className="movie-title">Mulan</h1>
-        <h3 className="trending">Now Trending</h3>
+    <section className="preview">
+      <div className="trending-container">
+        <img className="top-img" src="https://observer.case.edu/wp-content/uploads/2020/09/Mulan-900x506.jpg" alt="" />
+        <div className="info">
+          <h1 className="movie-title">Mulan</h1>
+          <h3 className="trending">Now Trending</h3>
+        </div>
       </div>
     </section>
   )
