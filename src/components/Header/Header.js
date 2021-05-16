77ce738a6d5bca9c50aa14f1383826b9a989cca2
schlanjo <schlanjo@gmail.com>
2021-05-16 12:31:30 -0600

    Set user menu btn, set initial background color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Header.css'
 
+import userIcon from './user_icon.svg'
+
 export default function Header() {
   return (
     <header>
-      <h3>Netflex</h3>
-      <div>
-        <input 
-          type='search'
-          name='search'
-          placeholder='Movie title'
-        />
-        <button>👤</button>
-      </div>
+      <h3 className='app-logo'>Netflex</h3>
+      <input 
+        type='search'
+        name='search'
+        placeholder='Movie title'
+      />
+      <button className='user-menu-btn'><img src={userIcon} alt='user icon' /></button>
     </header>
   )
 }
