fc591a1a4bf9b3b5f06074ad5d65879efc9cd872
Ashish Malla <ashishmalla45@gmail.com>
2021-05-15 20:17:32 -0600

    Add error handling for response

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,17 +2,24 @@ const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
 const apiCalls = {
   fetchAllMovies: async () => {
     const response = await fetch(`${baseURL}`)
-    return await response.json()
+    return await apiCalls.checkForError(response)
   },
 
   fetchAMovie: async id => {
     const response = await fetch(`${baseURL}/${id}`)
-    return await response.json()
+     return await apiCalls.checkForError(response)
   },
 
   fetchVideos: async id => {
     const response = await fetch(`${baseURL}/${id}/videos`)
-    return await response.json()
+     return await apiCalls.checkForError(response)
+  },
+
+  checkForError: response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    } 
+    return response.json()
   }
 
 }
