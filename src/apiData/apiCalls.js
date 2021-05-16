29f472ff344b2cd91cd324459574ae825e9fbc94
Ashish Malla <ashishmalla45@gmail.com>
2021-05-16 14:42:12 -0600

    Remove unnecessary fetch methods

--- src/apiData/apiCalls.js
+++ src/apiData/apiCalls.js
@@ -1,19 +1,5 @@
 const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2/'
 const apiCalls = {
-  fetchAllMovies: async () => {
-    const response = await fetch(`${baseURL}`)
-    return await apiCalls.checkForError(response)
-  },
-
-  fetchAMovie: async id => {
-    const response = await fetch(`${baseURL}/${id}`)
-     return await apiCalls.checkForError(response)
-  },
-
-  fetchVideos: async id => {
-    const response = await fetch(`${baseURL}/${id}/videos`)
-     return await apiCalls.checkForError(response)
-  },
   fetchApiData: async (query) => {
     const response = await fetch(`${baseURL}${query}`)
     return await apiCalls.checkForError(response)
