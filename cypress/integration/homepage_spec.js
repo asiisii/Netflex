0e87e7cb7e7b8afc250bd05927ea98269680a483
Ashish Malla <ashishmalla45@gmail.com>
2021-05-17 21:46:49 -0600

    Add testing for loading msg and poster array length

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -4,7 +4,27 @@ describe('Homepage', () => {
   })
 
   it('should retrieve all the movies', () => {
-    cy.contains('h3','Netflex')
-      // .get('input[type=text]')
+    cy.get('.loading')
+      .contains('💪Loading Your Movies💪')
+    cy.contains('.app-logo','Netflex')
+    //or
+    // cy.get('.app-logo').should('contain', 'Netflex')
+
+    cy.get('input[type=search]')
+      .should('be.visible') 
+    // can also write just [type=search]
+    cy.get('.user-menu-btn').find('img')
+      .should('be.visible')
+    cy.contains('.results-title','All Movies')
+    cy.get('.top-img')
+      .should('have.length', 1)
+      .should('be.visible')
+    cy.get('.poster-image')
+      .should('have.length', 10)
+      .should('be.visible')
+    // hover check 
+    // i gotta value 
   })
 })
+// use get when looking for a class
+// use contains to check if the DOM elem contains desired text
