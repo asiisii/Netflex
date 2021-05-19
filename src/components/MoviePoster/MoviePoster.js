c3bf067f08274fe8ccaee80d848a86db8522ec79
schlanjo <schlanjo@gmail.com>
2021-05-18 19:57:31 -0600

    Set up Link for each movie poster

--- src/components/MoviePoster/MoviePoster.js
+++ src/components/MoviePoster/MoviePoster.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './MoviePoster.css'
 
 export default function MoviePoster(props) {
   return (
-    <div id={props.id} className='movie-poster'>
+    <Link
+      to={`/movies/${props.id}`} 
+      id={props.id} 
+      className='movie-poster'
+    >
       <img 
-      className='poster-image'
-      src={props.image} 
-      alt={`${props.title} poster`} 
-      onClick={() => props.display(props.id)}
+        className='poster-image'
+        src={props.image} 
+        alt={`${props.title} poster`} 
+        onClick={() => props.display(props.id)}
       />
-    </div>
+    </Link>
   )
 }
