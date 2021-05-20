aa9cd3eec33d0d1c8e8d398e4b6cce1724882e37
Ashish Malla <ashishmalla45@gmail.com>
2021-05-19 21:13:50 -0600

    Add test for home page url and movie info page url

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,30 +1,50 @@
 describe('Homepage', () => {
+  
   beforeEach(() => {
     cy.checkAllMoviesData()
   })
+  describe('Onload', () => {
 
-  it('should retrieve all the movies', () => {
-    cy.get('.loading')
-      .contains('💪Loading Your Movies💪')
-    cy.contains('.app-logo','Netflex')
-    //or
-    // cy.get('.app-logo').should('contain', 'Netflex')
-
-    cy.get('input[type=search]')
-      .should('be.visible') 
-    // can also write just [type=search]
-    cy.get('.user-menu-btn').find('img')
-      .should('be.visible')
-    cy.contains('.results-title','All Movies')
-    cy.get('.top-img')
-      .should('have.length', 1)
-      .should('be.visible')
-    cy.get('.poster-image')
-      .should('have.length', 10)
-      .should('be.visible')
-    // hover check 
-    // i gotta value 
+    it('should let user know the movie list is loading', () => {
+      cy.get('.loading')
+        .contains('💪Loading Your Movies💪')
+    })
+  
+    it('should display header and preview img', () => {
+      cy.contains('.app-logo','Netflex')
+      cy.get('input[type=search]')
+        .should('be.visible') 
+      cy.get('.user-menu-btn').find('img')
+        .should('be.visible')
+      cy.get('.top-img')
+        .should('be.visible')
+    })
+  
+    it('should retrieve all the movies', () => {
+      cy.contains('.results-title','All Movies')
+      cy.get('.poster-image')
+        .should('have.length', 10)
+        .should('be.visible')
+    })
   })
+  
+  describe('Onclick', () => {
+    // beforeEach(() => {
+    //   cy.checkAllMoviesData()
+    // })
+
+    it('should have / path on load', () => {
+      cy.url().should('equal', 'http://localhost:3000/')
+    })
+
+    it('should have link path to specific movie on poster click', () => {
+      cy.get('.movie-poster')
+      cy.get('.poster-image')
+        .eq(0).click()
+        .url().should('equal', 'http://localhost:3000/movies/694919')
+    })
+
+  })
+ 
+
 })
-// use get when looking for a class
-// use contains to check if the DOM elem contains desired text
