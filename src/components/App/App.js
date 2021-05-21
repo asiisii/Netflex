44f7887f7de8d94dd37633a9cebfaa072b7cca8c
Ashish Malla <ashishmalla45@gmail.com>
2021-05-21 17:35:46 -0600

    Add getMoviesID method to filter movies id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,31 +10,63 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      movies: ''
+      moviesIDList: ''
     }
   }
-  getMovies = (movies) => {
-    console.log(movies);
+  getMoviesID = (movies) => {
+    // console.log(movies);
+    const movieIDs = movies.map(movie => movie.id)
+    console.log(movieIDs);
+  return this.setState({moviesIDList: movieIDs})
+    // console.log(this.state.moviesIDList);
+    // return movieIDs
+   
   }
   render() {
+    // const a = this.getMoviesID()
+    // console.log(this.state.moviesIDList);
+    const {moviesIDList} = this.state;
     return ( 
       <div className="app">
         <Switch>
         <Route
           exact path='/movies/:id'
           render={({ match }) => {
-            return (
-              <MovieInfo 
-                id={match.params.id}
-              />
-            ) 
+            const selectedID = parseInt(match.params.id, 10)
+            // {!moviesIDList && console.log('<><><>')}
+            // { moviesIDList ? <MovieInfo 
+            //       id={selectedID}
+            //     /> : console.log('<><><>') }
+            // const matchedID = moviesIDList.find(movie => movie === selectedID)
+            // console.log(matchedID);
+            // const id = matchedID ? matchedID : selectedID
+              // let a;
+              // if (this.state.moviesIDList) {
+              // a = this.state.moviesIDList.find(id => id === selectedID) 
+              // console.log(a);
+              // return a
+              // }
+              // console.log(a);
+              // if (!a) {
+              //   console.log('dammit');
+              //   return <h1>Page Not found</h1>
+              // } 
+              // else {
+                // console.log(a);
+              return (
+                <MovieInfo 
+                  id={match.params.id}
+                />
+              ) 
+              // } 
+            // }
           }}
         />
         <Route
           exact path='/'
           render={() => {
             return (
-              <Home handleClick={this.getMovies} />
+              <Home handleClick={this.getMoviesID} />
             )
           }}
         />
