10405da604d6ebb8d0e651ed71d4b312b31e0b85
Ashish Malla <ashishmalla45@gmail.com>
2021-05-20 23:12:41 -0600

    Change App functional to class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,38 +6,44 @@ import MovieInfo from '../MovieInfo/MovieInfo'
 
 import './App.css'
 
-function App() {
-
-  return ( 
-    <div className="app">
-      <Switch>
-      <Route
-        exact path='/movies/:id'
-        render={({ match }) => {
-          return (
-            <MovieInfo 
-              id={match.params.id}
-            />
-          ) 
-        }}
-      />
-      <Route
-        exact path='/'
-        render={() => {
-          return (
-            <Home handleClick={getMovies} />
-          )
-        }}
-      />
-      <Route render= {() => <h1>Page Not found</h1>} />
-      </Switch>
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: ''
+    }
+  }
+  getMovies = (movies) => {
+    console.log(movies);
+  }
+  render() {
+    return ( 
+      <div className="app">
+        <Switch>
+        <Route
+          exact path='/movies/:id'
+          render={({ match }) => {
+            return (
+              <MovieInfo 
+                id={match.params.id}
+              />
+            ) 
+          }}
+        />
+        <Route
+          exact path='/'
+          render={() => {
+            return (
+              <Home handleClick={this.getMovies} />
+            )
+          }}
+        />
+        <Route render= {() => <h1>Page Not found</h1>} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-const getMovies = (movies) => {
-  console.log(movies);
-  return movies;
-}
 
 export default App;
