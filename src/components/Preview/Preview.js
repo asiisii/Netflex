00607c0bea28a37652b62fd599f2bef73a8ba527
schlanjo <schlanjo@gmail.com>
2021-05-22 09:58:00 -0600

    Set desktop version of trending text

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -7,12 +7,10 @@ export default function Preview(props) {
       <div className="trending-container">
         <img className="top-img" src="https://observer.case.edu/wp-content/uploads/2020/09/Mulan-900x506.jpg" alt="" />
         <div className="info">
-          {/* <h1 className="movie-title">Mulan</h1>
-          <h3 className="trending">Now Trending</h3> */}
+          <h1 className="movie-title">Mulan</h1>
+          <h3 className="trending">Now Trending</h3>
         </div>
       </div>
     </section>
   )
 }
-
-
